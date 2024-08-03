24d7a133806899e5eb7278cc879508e38e2751fe
Shantanu1807 <shantanupatel140@gmail.com>
2024-08-04 00:47:46 +0530

    pushing code

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerDashboard from "./pages/BuyerDashboard";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element = {<Login/>}/>
+          <Route path="/signup" element = {<Signup/>}/>
+          <Route path="/seller/profile" element = {<SellerDashboard/>}/>
+          <Route path="/buyer/profile" element = {<BuyerDashboard/>}/>
+         
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
---
+++ client/src/pages/SellerDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SellerDashboard = () => {
+    return (
+        <div>SellerDashboard</div>
+    )
+}
